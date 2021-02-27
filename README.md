835e033af614bfeafe9a37ad945a73400f77468c
Elmo675 <40466868+Elmo675@users.noreply.github.com>
2021-02-27 17:39:57 +0100

    Starting Project
    Basic Structure

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple Rest Api with Maven, Spring Boot, Docker, Github, JRA, MySQL
---
+++ src/main/java/com/staxrt/tutorial/model/Post.java
@@ -0,0 +1,80 @@
+package com.staxrt.tutorial.model;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "posts")
+@EntityListeners(AuditingEntityListener.class)
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+
+    @Column(name = "data", nullable = false)
+    private String data;
+
+    @Column(name = "privacy", nullable = false)
+    private String privacy;
+
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false)
+    private Date createdAt;
+
+    @CreatedBy
+    @Column(name = "created_by", nullable = false)
+    private String createdBy;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+    private Date updatedAt;
+
+    @LastModifiedBy
+    @Column(name = "updated_by", nullable = false)
+    private String updatedBy;
+
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+
+    public String getData(){ return data; }
+    public void   setData(String data){this.data = data;}
+
+    public String getPrivacy(){ return privacy; }
+    public void   setPrivacy(String privacy){this.privacy=privacy;}
+
+    public Date getCreatedAt() { return createdAt; }
+    public void setCreatedAt(Date createdAt){ this.createdAt=createdAt;}
+
+    public String getCreatedBy(){ return createdBy; }
+    public void   setCreatedBy(String createdBy){ this.createdBy=createdBy;}
+
+    public Date getUpdatedAt(){ return updatedAt; }
+    public void setUpdatedAt(Date updatedAt){this.updatedAt=updatedAt;}
+
+    public String getUpdatedBy(){ return updatedBy;}
+    public void   setUpdatedBy(String updatedBy){this.updatedBy=updatedBy;}
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", data='" + data + '\'' +
+                ", privacy='" + privacy + '\'' +
+                ", createdAt=" + createdAt +
+                ", createdBy='" + createdBy + '\'' +
+                ", updatedAt=" + updatedAt +
+                ", updatedby='" + updatedBy + '\'' +
+                '}';
+    }
+}
